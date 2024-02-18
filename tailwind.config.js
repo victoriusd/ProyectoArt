923b35e0f0077e215f59791c733d73c15ed50c8b
Victor <victoriusd4@gmail.com>
2024-02-17 21:24:50 -0400

    v1
    creacion del nav | diseño telefono prioridad

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#6DF2CC',
+        secondary: '#13F2A1',
+        tertiary: '#404040',
+        // ...
+      },
+    },
+  },
+  plugins: [],
+}
+
