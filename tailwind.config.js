8ef7dacba869f0590a39d10e53dd61f0c48c0fa5
Victor <victoriusd4@gmail.com>
2024-02-19 18:48:07 -0400

    19/02
    Prueba de paleta 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,21 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        Geologica: ['Geologica']
+      },
       colors: {
-        primary: '#6DF2CC',
-        secondary: '#04DBA2',
-        tertiary: '#404040',
-        // ...
+        first: {
+          1: '#6DF2CC',
+          2: '#04DBA2',
+          3: '#404040',
+        },
+        second: {
+          1: '#8FDCC2',
+          2: '#233831',
+          3: '#D4EDE4',
+          4: '#588D7B'
+        }
       },
     },
   },
